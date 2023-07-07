3bb012d5b908049b3f8296109dad387176b00641
Sae Woo Nam <nams@nist.gov>
2023-07-07 17:07:54 -0600

    Initial commit

--- gh-pages.js
+++ gh-pages.js
@@ -4,10 +4,10 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository  
+        repo: 'git@github.com:saewoonam/first-dr-plots-log.git',
         user: {
-            name: 'Your name', // update to use your name
-            email: 'Your Email address' // Update to use your email
+            name: 'Sae Woo Nam', // update to use your name
+            email: 'saewoo.nam@nist.gov' // Update to use your email
         }
     },
     () => {
